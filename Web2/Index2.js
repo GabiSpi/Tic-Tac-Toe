1b470a726f85a73473a7a4392730eb8d1ed75c5e
GabiSpi <108688090+GabiSpi@users.noreply.github.com>
2023-02-04 09:22:07 -0300

    Add files via upload

---
+++ Web2/Index2.js
@@ -0,0 +1,122 @@
+var color;
+function cambiaColor1() {
+    if(color == 0){
+    document.getElementById("cuadrado1").style.backgroundColor = "lightblue";
+    color = 1;
+    } else{
+        document.getElementById("cuadrado1").style.backgroundColor = "green";
+        color = 0;
+    }
+  }
+  function cambiaColor2() {
+    
+  
+    if(color == 0){
+        document.getElementById("cuadrado2").style.backgroundColor = "lightblue";
+        color = 1;
+        } else{
+            document.getElementById("cuadrado2").style.backgroundColor = "green";
+            color = 0;
+        }
+    
+  }
+  function cambiaColor3() {
+    
+   
+    if(color == 0){
+        document.getElementById("cuadrado3").style.backgroundColor = "lightblue";
+        color = 1;
+        } else{
+            document.getElementById("cuadrado3").style.backgroundColor = "green";
+            color = 0;
+        }
+    
+    
+    
+  }
+  
+  function cambiaColor4() {
+    
+    
+    if(color == 0){
+        document.getElementById("cuadrado4").style.backgroundColor = "lightblue";
+        color = 1;
+        } else{
+            document.getElementById("cuadrado4").style.backgroundColor = "green";
+            color = 0;
+        }
+    
+    
+    
+  }
+  function cambiaColor5() {
+    
+    
+    if(color == 0){
+        document.getElementById("cuadrado5").style.backgroundColor = "lightblue";
+        color = 1;
+        } else{
+            document.getElementById("cuadrado5").style.backgroundColor = "green";
+            color = 0;
+        }
+    
+    
+    
+  }
+  function cambiaColor6() {
+    
+   
+    if(color == 0){
+        document.getElementById("cuadrado6").style.backgroundColor = "lightblue";
+        color = 1;
+        } else{
+            document.getElementById("cuadrado6").style.backgroundColor = "green";
+            color = 0;
+        }
+    
+    
+    
+  }
+
+  function cambiaColor7() {
+    
+   
+    if(color == 0){
+        document.getElementById("cuadrado7").style.backgroundColor = "lightblue";
+        color = 1;
+        } else{
+            document.getElementById("cuadrado7").style.backgroundColor = "green";
+            color = 0;
+        }
+    
+    
+    
+  }
+  function cambiaColor8() {
+    
+    
+    if(color == 0){
+        document.getElementById("cuadrado8").style.backgroundColor = "lightblue";
+        color = 1;
+        } else{
+            document.getElementById("cuadrado8").style.backgroundColor = "green";
+            color = 0;
+        }
+    
+    
+    
+  }
+  function cambiaColor9() {
+    
+    
+    if(color == 0){
+        document.getElementById("cuadrado9").style.backgroundColor = "lightblue";
+        color = 1;
+        } else{
+            document.getElementById("cuadrado9").style.backgroundColor = "green";
+            color = 0;
+        }
+    
+    
+    
+  }
